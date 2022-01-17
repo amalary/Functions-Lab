9125c5562b4ba0f697f22d10e7ae9c0450ff86c3
Anthony Malary <amalary@bu.edu>
2022-01-17 13:28:57 -0800

    Calculate function answered

--- functionsLab.js
+++ functionsLab.js
@@ -10,7 +10,30 @@ function reverseWordOrder(string){
     return text.reverse()
 }; 
 
-
-
-
 console.log(reverseWordOrder('Anthony me call'))
+
+function calculate(num1,num2,operation){
+
+    if(operation === 'add'){
+
+        return num1 + num2
+    }
+    else if(operation === 'multiply'){
+
+        return num1 * num2
+    }
+    else if (operation === 'subtract'){
+
+        return num1 - num2 
+    }
+    else if (operation ==='division'){
+
+        return num1/num2
+    }
+    else if (operation === 'exponent'){
+
+        return num1 ** num2
+    }
+}
+
+console.log(calculate(12,3,'division')); 
